60c8cf1a6389999c32babdbee44ddf6e6625c9b4
ChaochihL <liux1299@umn.edu>
2021-01-19 19:06:13 -0600

    use find instead of ls for large num subs files and fix redirect of stderr

--- 02_analysis/bad_mutations/bad_mut_predict.sh
+++ 02_analysis/bad_mutations/bad_mut_predict.sh
@@ -12,14 +12,14 @@ source activate /home/morrellp/liux1299/.conda/envs/bad_mutations
 # User provided input arguments
 # Full path to a list of lists (to utilize GNU parallel and job arrays)
 # List of lists here should only include FASTA files that had an alignment
-#	(i.e., .fa and .tree files were generated) in the previous align step
+#   (i.e., .fa and .tree files were generated) in the previous align step
 FASTA_LIST_OF_LISTS="$1"
 # Full path to the config file
 CONFIG_FILE="$2"
 # Full path to a list of MSA_Output directories that contain *.fa and *.tree files
 MSA_DIR_LIST="$3"
 # Full path to per transcript substitutions .subs files
-#	This output is from the VeP_to_Subs.py supporting script
+#   This output is from the VeP_to_Subs.py supporting script
 SUBS_DIR="$4"
 # Full path to a list of primary transcripts, one per line
 PRIMARY_TRANSCRIPTS="$5"
@@ -38,7 +38,7 @@ mkdir -p ${OUT_DIR} ${LOG_FILE_DIR} ${OUT_DIR}/all_predict_log
 
 # Preparation steps
 # Build array of substutions files
-SUBS_ARR=($(ls ${SUBS_DIR}/*.subs | sort -V))
+SUBS_ARR=($(find ${SUBS_DIR} -name "*.subs" | sort -V))
 # Build array of MSA_output subdirectories containing .fa and .tree files
 MSA_DIR_ARR=($(cat ${MSA_DIR_LIST}))
 
@@ -47,10 +47,10 @@ MSA_DIR_ARR=($(cat ${MSA_DIR_LIST}))
 PAIRED_ARR=()
 for i in ${SUBS_ARR[@]}
 do
-	for m in ${MSA_DIR_ARR[@]}
-	do
-		PAIRED_ARR+=("${i},${m}")
-	done
+    for m in ${MSA_DIR_ARR[@]}
+    do
+        PAIRED_ARR+=("${i},${m}")
+    done
 done
 
 # Determine maximum array limit
@@ -68,60 +68,60 @@ CURR_DIR_ID=$(basename ${CURR_MSA_DIR})
 CURR_FASTA_LIST=$(grep -w ${CURR_DIR_ID} ${FASTA_LIST_OF_LISTS})
 
 # Build array of MSA_output .fa and .tree files from current subdirectory (e.g., hvulgare_cds_list-000)
-#TRANSCRIPT_MSA_FASTA_ARR=($(find ${CURR_MSA_DIR} -name "*.fasta" | sort -V))
 TRANSCRIPT_MSA_FASTA_ARR=()
 for i in $(find ${CURR_MSA_DIR} -name "*.fasta" | sort -V)
 do
-	curr_fasta_name=$(basename ${i} .fasta)
-	# If current fasta file is a primary transcript, add to array for processing
-	if grep -qw ${curr_fasta_name} ${PRIMARY_TRANSCRIPTS}
-	then
-		TRANSCRIPT_MSA_FASTA_ARR+=("${i}")
-	fi
+    curr_fasta_name=$(basename ${i} .fasta)
+    # If current fasta file is a primary transcript, add to array for processing
+    if grep -qw ${curr_fasta_name} ${PRIMARY_TRANSCRIPTS}
+    then
+        TRANSCRIPT_MSA_FASTA_ARR+=("${i}")
+    fi
 done
 
 function predict_sub() {
-	local bad_mut_script="$1"
-	local config_file="$2"
-	local fasta_list="$3"
-	local msa_fasta="$4"
-	local curr_msa_dir="$5"
-	local subs_file="$6"
-	local out_dir="$7"
-	# Get name of MSA_output fasta file
-	msa_name=$(basename ${msa_fasta} .fasta)
-	# Get msa_tree file
-	msa_tree="${curr_msa_dir}/${msa_name}.tree"
-	tree_name=$(basename ${msa_tree} .tree)
+    local bad_mut_script="$1"
+    local config_file="$2"
+    local fasta_list="$3"
+    local msa_fasta="$4"
+    local curr_msa_dir="$5"
+    local subs_file="$6"
+    local out_dir="$7"
+    # Get name of MSA_output fasta file
+    msa_name=$(basename ${msa_fasta} .fasta)
+    # Get msa_tree file
+    msa_tree="${curr_msa_dir}/${msa_name}.tree"
+    tree_name=$(basename ${msa_tree} .tree)
 
-	# Check that msa_fasta and msa_tree names match
-	if [[ ${msa_name} != ${tree_name} ]]
-	then
-		echo "There is a mismatch between the MSA fasta ${msa_name} and tree file ${tree_name}, exiting..."
-		exit 31
-	fi
+    # Check that msa_fasta and msa_tree names match
+    if [[ ${msa_name} != ${tree_name} ]]
+    then
+        echo "There is a mismatch between the MSA fasta ${msa_name} and tree file ${tree_name}, exiting..."
+        exit 31
+    fi
 
-	# Get current list prefix
-	curr_list_prefix=$(basename ${fasta_list} .txt)
-	# Get fasta file that corresponds with MSA fasta file
-	fasta_file=$(grep -w ${msa_name} ${fasta_list})
-	# Get subs filename
-	subs_name=$(basename ${subs_file})
-	subdir_name="${curr_list_prefix}_${subs_name}"
-	# Check if out subdirectory exists, if not make it
-	mkdir -p ${out_dir}/${subdir_name} ${out_dir}/all_predict_log/${subdir_name}
+    # Get current list prefix
+    curr_list_prefix=$(basename ${fasta_list} .txt)
+    # Get fasta file that corresponds with MSA fasta file
+    fasta_file=$(grep -w ${msa_name} ${fasta_list})
+    # Get subs filename
+    subs_name=$(basename ${subs_file})
+    subdir_name="${curr_list_prefix}_${subs_name}"
+    # Check if out subdirectory exists, if not make it
+    mkdir -p ${out_dir}/${subdir_name} ${out_dir}/all_predict_log/${subdir_name}
 
-	# Predict substitutions
-	echo "Start predict step: ${out_dir}/${subdir_name}."
-	python ${bad_mut_script} predict \
-		-c ${config_file} \
-		-f ${fasta_file} \
-		-a ${msa_fasta} \
-		-r ${msa_tree} \
-		-s ${subs_file} \
-		-o ${out_dir}/${subdir_name} \
-        2> ${out_dir}/all_predict_log/${subdir_name}/${msa_name}_predict.log
-	echo "Done: ${out_dir}/${subdir_name}"
+    # Predict substitutions
+    # Redirect only stdout to log file and NOT stderr
+    echo "Start predict step: ${out_dir}/${subdir_name}."
+    python ${bad_mut_script} predict \
+        -c ${config_file} \
+        -f ${fasta_file} \
+        -a ${msa_fasta} \
+        -r ${msa_tree} \
+        -s ${subs_file} \
+        -o ${out_dir}/${subdir_name} \
+        1> ${out_dir}/all_predict_log/${subdir_name}/${msa_name}_predict.log
+    echo "Done: ${out_dir}/${subdir_name}"
 }
 
 export -f predict_sub
