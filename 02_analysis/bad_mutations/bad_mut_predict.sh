5ce2e9dcdf924174a1d95548a07d72b346e1d467
ChaochihL <liux1299@umn.edu>
2021-03-17 14:13:18 -0500

    fix error, add additional checks, and moved dependencies to job script

--- 02_analysis/bad_mutations/bad_mut_predict.sh
+++ 02_analysis/bad_mutations/bad_mut_predict.sh
@@ -3,12 +3,6 @@
 set -e
 set -o pipefail
 
-module load parallel
-module load python3/3.6.3_anaconda5.0.1
-
-# Activate conda environment
-source activate /home/morrellp/liux1299/.conda/envs/bad_mutations
-
 # User provided input arguments
 # Full path to a list of lists (to utilize GNU parallel and job arrays)
 # List of lists here should only include FASTA files that had an alignment
@@ -33,52 +27,6 @@ BAD_MUT_SCRIPT="$8"
 LOG_FILE_DIR="$9"
 
 #------------------------------
-# Check if out dir exist, if not make them
-mkdir -p ${OUT_DIR} ${LOG_FILE_DIR} ${OUT_DIR}/all_predict_log
-
-# Preparation steps
-# Build array of substutions files
-SUBS_ARR=($(find ${SUBS_DIR} -name "*.subs" | sort -V))
-# Build array of MSA_output subdirectories containing .fa and .tree files
-MSA_DIR_ARR=($(cat ${MSA_DIR_LIST}))
-
-# Each task array index will be a pair of one .subs file and current subdirectory (e.g., hvulgare_cds_list-000)
-# Prepare tab delimited file containing subs file followed by current subdirectory
-PAIRED_ARR=()
-for i in ${SUBS_ARR[@]}
-do
-    for m in ${MSA_DIR_ARR[@]}
-    do
-        PAIRED_ARR+=("${i},${m}")
-    done
-done
-
-# Determine maximum array limit
-MAX_ARRAY_LIMIT=$[${#PAIRED_ARR[@]} - 1]
-echo "Maximum array limit is ${MAX_ARRAY_LIMIT}."
-
-# Get current subs file
-CURR_SUBS_FILE=$(echo ${PAIRED_ARR[${SLURM_ARRAY_TASK_ID}]} | cut -d"," -f 1)
-# Get current MSA_output subdirectory we are processing
-CURR_MSA_DIR=$(echo ${PAIRED_ARR[${SLURM_ARRAY_TASK_ID}]} | cut -d"," -f 2)
-echo "Current .subs file: ${CURR_SUBS_FILE}"
-echo "Current subdirectory we are processing: ${CURR_MSA_DIR}"
-
-CURR_DIR_ID=$(basename ${CURR_MSA_DIR})
-CURR_FASTA_LIST=$(grep -w ${CURR_DIR_ID} ${FASTA_LIST_OF_LISTS})
-
-# Build array of MSA_output .fa and .tree files from current subdirectory (e.g., hvulgare_cds_list-000)
-TRANSCRIPT_MSA_FASTA_ARR=()
-for i in $(find ${CURR_MSA_DIR} -name "*.fasta" | sort -V)
-do
-    curr_fasta_name=$(basename ${i} .fasta)
-    # If current fasta file is a primary transcript, add to array for processing
-    if grep -qw ${curr_fasta_name} ${PRIMARY_TRANSCRIPTS}
-    then
-        TRANSCRIPT_MSA_FASTA_ARR+=("${i}")
-    fi
-done
-
 function check_filepaths() {
     local curr_file="$1"
     # If file doesn't exist, return error and exit
@@ -91,13 +39,72 @@ function check_filepaths() {
 
 export -f check_filepaths
 
+# Check if out dir exist, if not make them
+mkdir -p ${OUT_DIR} ${LOG_FILE_DIR} ${OUT_DIR}/all_predict_log
+
+# Preparation steps
+# Generate list of substitutions files
+find ${SUBS_DIR} -name "*.subs" | sort -V > ${OUT_DIR}/subs_list.txt
+
+# Intersect primary transcripts and substitutions files
+if [ -f ${OUT_DIR}/primary_transcript_subs_list.txt ]
+then
+    echo "Primary transcript subs file list exists, proceeding with current list..."
+else
+    echo "Intersecting primary transcript names and substitutions files"
+    for pt in $(cat ${PRIMARY_TRANSCRIPTS})
+    do
+        if grep -wq ${pt} ${OUT_DIR}/subs_list.txt
+        then
+            # Primary transcript name is present in subs_list.txt
+            grep -w ${pt} ${OUT_DIR}/subs_list.txt >> ${OUT_DIR}/primary_transcript_subs_list.txt
+            # Also save just the primary transcript name due to naming scheme
+            #   (e.g., primary transcript is phvul.001g001100.1 and subs file is phvul.001g001100.1.v2.1.subs)
+            echo ${pt} >> ${OUT_DIR}/primary_transcript_intersect_names_only.txt
+        fi
+    done
+fi
+
+# Build array of substutions files from primary transcripts only
+PT_SUBS_ARR=($(cat ${OUT_DIR}/primary_transcript_intersect_names_only.txt))
+# Build array of MSA_output subdirectories containing .fa and .tree files
+# Each task array index will be one current subdirectory (e.g., hvulgare_cds_list-000)
+MSA_DIR_ARR=($(cat ${MSA_DIR_LIST}))
+
+# Determine maximum array limit
+MAX_ARRAY_LIMIT=$[${#MSA_DIR_ARR[@]} - 1]
+echo "Maximum array limit is ${MAX_ARRAY_LIMIT}."
+
+# Get current MSA_output subdirectory we are processing in current task array
+CURR_MSA_DIR=$(echo ${MSA_DIR_ARR[${SLURM_ARRAY_TASK_ID}]})
+echo "Current subdirectory we are processing in task array index ${SLURM_ARRAY_TASK_ID}: ${CURR_MSA_DIR}"
+CURR_DIR_ID=$(basename ${CURR_MSA_DIR})
+CURR_FASTA_LIST=$(grep -w ${CURR_DIR_ID} ${FASTA_LIST_OF_LISTS})
+
+# Build array of MSA_output .fa and .tree files from current subdirectory (e.g., hvulgare_cds_list-000)
+MSA_FASTA_ARR=()
+for i in $(find ${CURR_MSA_DIR} -name "*.fasta" | sort -V)
+do
+    transcript_prefix=$(basename ${i} .fasta)
+    # Match current fasta with primary transcript and subs files
+    # Check if PT_SUBS_ARR contains current transcript fasta prefix, if so add to array for processing
+    if [[ "${PT_SUBS_ARR[@]}" =~ "${transcript_prefix}" ]]
+    then
+        # Check to make sure .fasta and .tree files exist
+        check_filepaths "${CURR_MSA_DIR}/${transcript_prefix}.fasta"
+        # Add to array
+        # Current fasta prefix is present in PT_SUBS_ARR
+        MSA_FASTA_ARR+=("${CURR_MSA_DIR}/${transcript_prefix}.fasta")
+    fi
+done
+
 function predict_sub() {
     local bad_mut_script="$1"
     local config_file="$2"
     local fasta_list="$3"
     local msa_fasta="$4"
     local curr_msa_dir="$5"
-    local subs_file="$6"
+    local subs_list="$6"
     local out_dir="$7"
     # Get name of MSA_output fasta file
     msa_name=$(basename ${msa_fasta} .fasta)
@@ -117,10 +124,10 @@ function predict_sub() {
     # Get fasta file that corresponds with MSA fasta file
     fasta_file=$(grep -w ${msa_name} ${fasta_list})
     # Get subs filename
-    subs_name=$(basename ${subs_file})
-    subdir_name="${curr_list_prefix}_${subs_name}"
+    subs_file=$(grep -w ${msa_name} ${subs_list})
+    subdir_name="${curr_list_prefix}"
     # Check if out subdirectory exists, if not make it
-    mkdir -p ${out_dir}/${subdir_name} ${out_dir}/all_predict_log/${subdir_name}
+    mkdir -p ${out_dir}/${subdir_name} ${out_dir}/all_predict_log/${subdir_name} ${out_dir}/${subdir_name}/problematic_predictions
 
     # Check that all files exist
     check_filepaths ${config_file}
@@ -131,6 +138,7 @@ function predict_sub() {
 
     # Predict substitutions
     # Redirect only stdout to log file and NOT stderr
+    set -x # Start debugging
     echo "Start predict step: ${out_dir}/${subdir_name}."
     python ${bad_mut_script} predict \
         -c ${config_file} \
@@ -140,6 +148,7 @@ function predict_sub() {
         -s ${subs_file} \
         -o ${out_dir}/${subdir_name} \
         1> ${out_dir}/all_predict_log/${subdir_name}/${msa_name}_predict.log
+    set +x # End debugging
     # Stricter error checking so when predict file doesn't get written, the job doesn't
     #   return a zero exit status
     # Currently (2021-01-19), the predict output files should have the naming scheme: ${msa_name}_Predictions.txt
@@ -147,7 +156,18 @@ function predict_sub() {
     if [[ -f ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt ]]
     then
         # Predict output file got written
-        echo "Done: ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt"
+        echo "Predict output file was written: ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt"
+        echo "Checking predict output file..."
+        # More in depth check of predict file in cases where file was written but error still occured.
+        #   e.g., "Error: HyPhy killed by signal 15", "Function call stack", etc.
+        if grep -wq "Error" ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt
+        then
+            # Move problematic prediction to subdirectory for easy troubleshooting
+            mv ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt ${out_dir}/${subdir_name}/problematic_predictions
+            echo "The following prediction resulted in an error: ${out_dir}/${subdir_name}/problematic_predictions/${msa_name}_Predictions.txt"
+            echo "Please troubleshoot, exiting..."
+            exit 1
+        fi
     else
         # Predict output file failed to get written to file, exiting
         echo "The following predict output file didn't successfully get written to a file: ${out_dir}/${subdir_name}/${msa_name}_Predictions.txt"
@@ -161,4 +181,4 @@ export -f predict_sub
 # Parallelize across array of paired MSA_output fasta and tree files
 # Keep job log for parallel processes so upon resubmitting job, parallel can just re-run
 #   samples that don't have an exit status of 0.
-parallel --resume-failed --joblog ${LOG_FILE_DIR}/bad_mut_predict.sh.${SLURM_ARRAY_TASK_ID}.log predict_sub ${BAD_MUT_SCRIPT} ${CONFIG_FILE} ${CURR_FASTA_LIST} {} ${CURR_MSA_DIR} ${CURR_SUBS_FILE} ${OUT_DIR} ::: ${TRANSCRIPT_MSA_FASTA_ARR[@]}
+parallel --resume-failed --joblog ${LOG_FILE_DIR}/bad_mut_predict.sh.${SLURM_ARRAY_TASK_ID}.log predict_sub ${BAD_MUT_SCRIPT} ${CONFIG_FILE} ${CURR_FASTA_LIST} {} ${CURR_MSA_DIR} ${OUT_DIR}/primary_transcript_subs_list.txt ${OUT_DIR} ::: ${MSA_FASTA_ARR[@]}
