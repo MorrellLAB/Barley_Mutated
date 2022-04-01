53a66d79aa25d594c56fb9e86f5ff1e23aeec290
ChaochihL <liux1299@umn.edu>
2022-04-01 15:42:08 -0500

    update script

--- 00_sequence_processing/samplot-Morex_10x_nanopore_pacbio.sh
+++ 00_sequence_processing/samplot-Morex_10x_nanopore_pacbio.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --mem=30gb
 #SBATCH --tmp=22gb
-#SBATCH -t 03:00:00
+#SBATCH -t 08:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=liux1299@umn.edu
 #SBATCH -p small,ram256g,ram1t
@@ -35,7 +35,8 @@ BAM3="/panfs/roc/groups/9/morrellp/shared/Datasets/Alignments/pacbio_morex/pacbi
 # This bed file lists all intervals with 'Ns' in the reference genome
 ANNOTATE="/panfs/roc/groups/9/morrellp/pmorrell/Workshop/Barley_Morex_V2_pseudomolecules_parts_missing.bed.gz"
 # Type of variant
-VAR_TYPE="DEL"
+VAR_TYPE1="DEL"
+VAR_TYPE2="INS"
 
 #----------------
 mkdir -p ${OUT_DIR}
@@ -74,5 +75,33 @@ export -f run_samplot
 
 # Reformat regions line and store in array
 bed_arr=($(sed -e 's,\t,;,g' ${REGIONS}))
-
-parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr[@]}"
+# Get the number of regions
+total_lines=$(echo ${#bed_arr[@]})
+# If total number of lines exceeds 53,083, we'll need to split the
+#   bed_arr into batches otherwise we'll get the following error from GNU parallel:
+#   bash: /panfs/roc/msisoft/parallel/20210822/bin/parallel: Argument list too long
+# We'll use 50000 for a nicer number
+if [ "${total_lines}" -gt 50000 ]; then
+    echo "Total number of regions exceeds 50000. Splitting GNU parallel job into batches of regions."
+    # Split bed_arr into batches
+    bed_arr_batch1=($(sed -n "1,50000p" ${REGIONS} | sed -e 's,\t,;,g'))
+    bed_arr_batch2=($(sed -n "50001,${total_lines}p" ${REGIONS} | sed -e 's,\t,;,g'))
+    # Check the number of lines in each batch
+    echo "Batch1: ${#bed_arr_batch1[@]}"
+    echo "Batch2: ${#bed_arr_batch2[@]}"
+    # VAR_TYPE1, batch1
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE1}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr_batch1[@]}"
+    # VAR_TYPE1, batch2
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE1}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr_batch2[@]}"
+    # VAR_TYPE2, batch1
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE2}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr_batch1[@]}"
+    # VAR_TYPE2, batch2
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE2}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr_batch2[@]}"
+else
+    echo "Total number of regions doesn't exceed 50000. Running GNU parallel job as a single batch."
+    # Run as a single batch
+    # VAR_TYPE1
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE1}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr[@]}"
+    # VAR_TYPE2
+    parallel --verbose run_samplot "${REGIONS}" "${OUT_DIR}" "${ANNOTATE}" "${VAR_TYPE2}" {} ${SAMPLE_ID1} ${SAMPLE_ID2} ${SAMPLE_ID3} ${BAM1} ${BAM2} ${BAM3} ::: "${bed_arr[@]}"
+fi
