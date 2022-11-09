5da51e6b09891589f126bf7692b0179a8e12358d
ChaochihL <liux1299@umn.edu>
2022-11-09 16:05:34 -0600

    move shared functions to loadable script

--- 02_analysis/mutation_motif/prep_counts_table-hybrid_rare_snps.sh
+++ 02_analysis/mutation_motif/prep_counts_table-hybrid_rare_snps.sh
@@ -21,6 +21,8 @@ module load vcflib_ML/1.0.0_rc2
 module load bedtools/2.29.2
 # Conda environment for mutation motif software
 source activate /panfs/jay/groups/9/morrellp/liux1299/.conda/envs/mut_motif_env
+# Load shared functions
+source ~/GitHub/Barley_Mutated/02_analysis/mutation_motif/prep_counts_functions.sh
 
 # User provided input arguments
 VCF="/panfs/jay/groups/9/morrellp/shared/Projects/Mutant_Barley/hybrid_rare_vcfs/hybrid13.SNPs.rare.vcf.gz"
@@ -40,61 +42,6 @@ OUT_DIR="/panfs/jay/groups/9/morrellp/shared/Projects/Mutant_Barley/results/muta
 TEMP_DIR="/scratch.global/liux1299/temp_mut_motif.hybrid13.SNPs.rare"
 
 #----------------------
-# Define functions
-function vcf_to_bed() {
-    local vcf="$1"
-    local out_dir="$2"
-    local ref_fai="$3"
-    local win_flank_size="$4"
-    # Convert VCF to BED format using vcflib's vcf2bed.py
-    if [[ "${vcf}" == *"gz"* ]]; then
-        # We are working with gzipped VCF file
-        out_prefix=$(basename ${vcf} .vcf.gz)
-        zcat ${vcf} | vcf2bed.py > ${out_dir}/${out_prefix}.bed
-    else
-        # We are working with uncompressed vcf
-        out_prefix=$(basename ${vcf} .vcf)
-        cat ${vcf} | vcf2bed.py > ${out_dir}/${out_prefix}.bed
-    fi
-    # Add flanking interval in each direction
-    bedtools slop -i ${out_dir}/${out_prefix}.bed -g ${ref_fai} -b ${win_flank_size} > ${out_dir}/${out_prefix}.winFlank${win_flank_size}bp.bed
-}
-
-export -f vcf_to_bed
-
-function bed_to_fasta() {
-    local bed_file="$1"
-    local ref_fasta="$2"
-    local out_dir="$3"
-    # Prepare output file prefix
-    out_prefix=$(basename ${bed_file} .bed)
-    # Extract sequences from fasta as defined in BED
-    bedtools getfasta -fi ${ref_fasta} -bed ${bed_file} -fo ${out_dir}/${out_prefix}.fasta
-}
-
-export -f bed_to_fasta
-
-function run_aln_to_counts() {
-    local fasta_file="$1"
-    local out_dir="$2"
-    local flank_size="$3"
-    local out_prefix="$4"
-    local win_flank_size="$5"
-    # Prepare log file prefix
-    #log_prefix=$(basename ${fasta_file} .fasta)
-    # Prepare direction
-    curr_direction=$(basename ${fasta_file} _${out_prefix}.winFlank${win_flank_size}bp.fasta)
-    # Prepare counts table format required by mutation motif
-    aln_to_counts --align_path "${fasta_file}" \
-        --output_path "${out_dir}" \
-        --flank_size "${flank_size}" \
-        --direction "${curr_direction}" \
-        --force_overwrite
-        #--force_overwrite &> ${out_dir}/log_files/${log_prefix}.log
-}
-
-export -f run_aln_to_counts
-
 # Create output directories and subdirectories
 mkdir -p ${OUT_DIR} ${OUT_DIR}/counts_tables
 mkdir -p ${TEMP_DIR} ${TEMP_DIR}/split_vcf ${TEMP_DIR}/split_bed ${TEMP_DIR}/split_fasta
