7c45aab2da12d651d87fe221b09b23ce8b939b26
ChaochihL <liux1299@umn.edu>
2022-11-28 10:14:18 -0600

    change to csi indexing

--- 00_sequence_processing/Nanopore/add_RG_header-ipk_morex_85x_ont.sh
+++ 00_sequence_processing/Nanopore/add_RG_header-ipk_morex_85x_ont.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --mem=12gb
-#SBATCH --tmp=8gb
-#SBATCH -t 05:00:00
+#SBATCH --mem=8gb
+#SBATCH --tmp=6gb
+#SBATCH -t 12:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=liux1299@umn.edu
 #SBATCH -p small,ram256g,ram1t
@@ -32,4 +32,4 @@ OUT_PREFIX=$(basename ${BAM} .bam)
 # Add @RG header line to bam
 samtools addreplacerg -r ${RG_LINE} --threads ${NUM_THREADS} -o ${OUT_DIR}/${OUT_PREFIX}_wRG.bam ${BAM}
 # Index SAM
-samtools index -b ${OUT_DIR}/${OUT_PREFIX}_wRG.bam
+samtools index -c ${OUT_DIR}/${OUT_PREFIX}_wRG.bam
